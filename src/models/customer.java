05cbaa32926f5570bea2ddbff010911616d985fc
Biruktawit Yaze <biruktawityaze@gmail.com>
2026-01-29 13:49:02 -0900

    Update customer.java

--- src/models/customer.java
+++ src/models/customer.java
@@ -2,7 +2,6 @@ package model;
 
 public class Customer extends User {
 
-    // Constructor
     public Customer(String id, String name, String email, String phone, String password) {
         super(id, name, email, phone, password);
     }
