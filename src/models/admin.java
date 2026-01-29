d318b0e4c3a32430fb7d6628c6ae014b527e5205
Biruktawit Yaze <biruktawityaze@gmail.com>
2026-01-29 13:47:14 -0900

    Add constructor to Admin class

--- src/models/admin.java
+++ src/models/admin.java
@@ -2,7 +2,7 @@ package model;
 
 public class Admin extends User {
 
-    // Constructor
+    
     public Admin(String id, String name, String email, String phone, String password) {
         super(id, name, email, phone, password);
     }
