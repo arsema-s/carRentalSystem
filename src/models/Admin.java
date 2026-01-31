05798e4e867b0724d9bbc88c9dae4edfd4c4fd9f
Biruktawit Yaze <biruktawityaze@gmail.com>
2026-01-31 05:13:01 -0900

    Clean up Admin.java by removing blank spaces
    Removed unnecessary blank lines in Admin class.

--- src/models/Admin.java
+++ src/models/Admin.java
@@ -2,7 +2,6 @@ package model;
 
 public class Admin extends User {
 
-    
     public Admin(String id, String name, String email, String phone, String password) {
         super(id, name, email, phone, password);
     }
@@ -11,7 +10,6 @@ public class Admin extends User {
     public String getRole() {
         return "Admin";
     }
-
     @Override
     public String toString() {
         return "Admin{" +
